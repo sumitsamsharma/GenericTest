86be376a26dde0e7d35c0ab3682dc39de59757f2
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-16 11:21:20 +0530

    Added maximum float value test function

--- test/java/GenericTest.java
+++ test/java/GenericTest.java
@@ -4,10 +4,17 @@ import org.junit.Test;
 public class GenericTest
 {
     @Test
-    public void maximumTest(){
+    public void maxIntTest(){
         Integer x= 5, y=6, z=7;
         GenericMethod max=new GenericMethod();
         Assert.assertEquals(7,max.maxInt(x,y,z));
     }
+    @Test
+    public void maxFloatTest(){
+
+        GenericMethod max1=new GenericMethod();
+        Assert.assertEquals(7.1f,max1.maxFloat(6.5f,6.7f,7.1f),0.0f);
+    }
+
 
 }
